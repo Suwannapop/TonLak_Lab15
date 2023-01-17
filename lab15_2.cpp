d26865dbcd9691e62a0bd31856fc36a5d8fdeae9
Suppakorn <suwannapop01@gmail.com>
2023-01-18 00:11:44 +0700

    Finaly

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -38,9 +38,9 @@ void findLocalMax(const double A[][N], bool B[][N]){
 		{
 
 			if (A[i][j]>=A[i][j-1] && A[i][j]>=A[i][j+1]&& A[i][j]>= A[i-1][j]&& A[i][j]>=A[i+1][j]){
-				B[i][j] = true ;
+				B[i][j] = 1 ;
 			}else {
-				B[i][j] = false ;
+				B[i][j] = 0 ;
 			}
 			
 		}
@@ -51,9 +51,9 @@ void findLocalMax(const double A[][N], bool B[][N]){
 		for (int j = 0; j <= N; j++)
 		{
 
-			if (i==0 || i == 4 || j == 0 || j == 4)
+			if (i==0 || i == N-1 || j == 0 || j == N-1)
 			{
-				B[i][j] = false ;
+				B[i][j] = 0 ;
 			}
 			
 		}	
